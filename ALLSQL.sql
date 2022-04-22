936223afb526baffdf03e039517fc4ee7ed17865
Karamjeetroy <104204458+Karamjeetroy@users.noreply.github.com>
2022-04-22 17:19:05 +0530

    Update ALLSQL.sql

--- ALLSQL.sql
+++ ALLSQL.sql
@@ -1,1 +1,6 @@
-
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP 1000 [EmpID] NOT NULL
+      ,[Name]
+      ,[Gender]
+      ,[whats number]
+  FROM [db2].[dbo].[NewEmployees]
